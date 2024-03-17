76a1bea5c8900b7fb4537c61d14d3ab663cc9ac0
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-16 19:36:33 -0500

    Implemented deleting student

--- src/main/java/com/springproject/cruddemo/CruddemoApplication.java
+++ src/main/java/com/springproject/cruddemo/CruddemoApplication.java
@@ -25,9 +25,17 @@ public class CruddemoApplication {
 			// readStudent(studentDAO);
 			// queryForStudents(studentDAO);
 			// queryForStudentsByLastName(studentDAO);
+			// updateStudent(studentDAO);
 
-			updateStudent(studentDAO);
+			deleteStudent(studentDAO);
 		};
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+
+		int studentId = 1;
+		System.out.println("Deleting student id: " + studentId);
+		studentDAO.delete(studentId);
 	}
 
 	private void updateStudent(StudentDAO studentDAO) {
